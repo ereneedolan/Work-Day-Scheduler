e753c7e3294c105ccec5df391a8024e5537dd00c
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-06 23:01:42 -0400

    coding in javascript

--- script.js
+++ script.js
@@ -10,19 +10,25 @@
     // each timeblock is a button that can click into the hour
     // once in each timeblock the user can input an event
     //the text input for each event should then be saved in local storage
-// save button is a button saying "save event" that will save the information entered by the user
+// save button is a button showing a floppy disk that will save the information entered by the user
+    // the button will be at the end of each row 
     
-
 
 // DATA / STATE
 
 var saveBtn = document.getElementById("save-event");
 
+var eventRows = $("<div>").addclass();
+
+var listedHours = $("<div>").addclass().text();
+
 // FUNCTIONS
 
 
 //need to use military time so the computer can differentiate AM and PM
-for(i=0 i<18 i++)
+//business hours 9am- 5pm use military time of 9 - 18(5pm plus one for index)
+
+for(i=9 i<18 i++)
 
 if(i<12){
     hourDisplay = i + "AM";
